1a8b154ef374d478caca9335122aa36cfcdadf13
Max <122821653+J0kk1n@users.noreply.github.com>
2023-02-19 04:15:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+document.querySelector('.btn').addEventListener('click', rotate)
+let $img = document.querySelector('.lll')
+let $img1 = document.querySelector('.ggg')
+
+
+function rotate() {
+    $img.classList = 'img'
+    $img1.classList = 'img1'
+}
